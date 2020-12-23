0f5f3e3bed32ab8cdf41643823abcbbc9fa8163e
Varun Tangri <varuntangri@outlook.com>
2020-12-23 02:49:35 +0100

    some more modification for java code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
 
-      public static void main(String[] args){
-	  System.out.println("HelloWorld!! Its Me Varun!!");
+       public static void main(String[] args) {
+		  
+		  String name = "Varun";
+		  String pretext = "My name is";
+		  String greet = "Hello World!!";
+
+	  System.out.println(greet + "," + pretext + " " + name);
+	  //System.out.println("HelloWorld!! Its Me Varun!!");
 	  }
 	  
 }
